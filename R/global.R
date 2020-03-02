d3a9616ad8d11b936f04d78cc848ab93631c6b92
Stuart Wheater <stuart.wheater@arjuna.com>
2020-03-02 15:54:17 +0000

    Added 'R/global.R' for defining of global variables

---
+++ R/global.R
@@ -0,0 +1,3 @@
+# Define the global variables 'offset.to.use', 'weights.to.use', 'current.factor', 'factor1.levels' and 'factor2.levels'
+
+utils::globalVariables(c('offset.to.use', 'weights.to.use', 'current.factor', 'factor1.levels', 'factor2.levels'))
