c673d357da853fd46a8f63093768de9f4afb024e
Stuart Wheater <stuart.wheater@arjuna.com>
2021-01-04 13:45:21 +0000

    Updated to 'pos' parameter of 'get'

--- R/whereDS.R
+++ R/whereDS.R
@@ -10,7 +10,7 @@ whereDS <- function(variable.name = NULL, envir = rlang::caller_env(), class.typ
   else if (exists(variable.name,envir))
   {
     # Success case
-    variable.found <- get(variable.name, pos = position)
+    variable.found <- get(variable.name, pos = 1)
     if(class(variable.found) == class.type)
     {
       return(envir)
