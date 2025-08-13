c81e5ed10c0cd40779597b0949cf4795726361db
Stuart Wheater <stuart.wheater@arjuna.com>
2025-08-13 23:34:34 +0100

    Switch 'old_seed' to '.old_seed'

--- R/rangeDS.R
+++ R/rangeDS.R
@@ -10,8 +10,8 @@
 rangeDS <- function(xvect) {
   # back-up current .Random.seed and revert on.exit
   if (exists(x = ".Random.seed", envir = globalenv())) {
-      assign(x = "old_seed", value = .Random.seed, envir = parent.frame());
-      on.exit({ assign(x = ".Random.seed", value = parent.frame()$old_seed, envir = globalenv()); remove("old_seed", envir = parent.frame()) }, add = TRUE)
+      assign(x = ".old_seed", value = .Random.seed, envir = parent.frame());
+      on.exit({ assign(x = ".Random.seed", value = parent.frame()$.old_seed, envir = globalenv()); remove(".old_seed", envir = parent.frame()) }, add = TRUE)
   } else
       on.exit(if (exists(x = ".Random.seed", envir = globalenv())) remove(".Random.seed", envir = globalenv()), add = TRUE)
 
