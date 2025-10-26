aa9fb8f1005801b35e989bee3727762253e36b4d
Stuart Wheater <stuart.wheater@arjuna.com>
2025-10-26 19:32:40 +0000

    Updates for context() + copyright

--- tests/testthat/test-smk-colnamesDS.R
+++ tests/testthat/test-smk-colnamesDS.R
@@ -1,5 +1,6 @@
 #-------------------------------------------------------------------------------
 # Copyright (c) 2019-2022 University of Newcastle upon Tyne. All rights reserved.
+# Copyright (c) 2022-2025 Arjuna Technologies, Newcastle upon Tyne. All rights reserved.
 #
 # This program and the accompanying materials
 # are made available under the terms of the GNU Public License v3.0.
@@ -12,13 +13,13 @@
 # Set up
 #
 
-context("colnamesDS::smk::setup")
+# context("colnamesDS::smk::setup")
 
 #
 # Tests
 #
 
-context("colnamesDS::smk::data.frame")
+# context("colnamesDS::smk::data.frame")
 test_that("simple colnamesDS, data.frame", {
     input <- data.frame(v1 = c(0.0, 1.0, 2.0, 3.0, 4.0), v2 = c(4.0, 3.0, 2.0, 1.0, 0.0))
 
@@ -30,7 +31,7 @@ test_that("simple colnamesDS, data.frame", {
     expect_true("v2" %in% res)
 })
 
-context("colnamesDS::smk::data.matrix")
+# context("colnamesDS::smk::data.matrix")
 test_that("simple colnamesDS, data.matrix", {
     input <- data.matrix(data.frame(v1 = c(0.0, 1.0, 2.0, 3.0, 4.0), v2 = c(4.0, 3.0, 2.0, 1.0, 0.0)))
 
@@ -46,6 +47,6 @@ test_that("simple colnamesDS, data.matrix", {
 # Done
 #
 
-context("colnamesDS::smk::shutdown")
+# context("colnamesDS::smk::shutdown")
 
-context("colnamesDS::smk::done")
+# context("colnamesDS::smk::done")
